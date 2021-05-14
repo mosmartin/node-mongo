8a9fc1f60f547e7dbfc477c15b629a0d05f6a141
Martin Macharia <mosmartin5@gmail.com>
2021-05-14 16:26:36 +0200

    Create a listing

--- index.js
+++ index.js
@@ -2,27 +2,55 @@ require("dotenv").config();
 const { MongoClient } = require("mongodb");
 
 async function main() {
+  // mongo db uri
   const uri = process.env.MONGODB_URI;
 
+  // instantiate a new client
   const client = new MongoClient(uri, { useUnifiedTopology: true });
 
   try {
+    // establish a connection to the database
     await client.connect();
 
-    await listDatabases(client);
+    // get list of all databases
+    // await listDatabases(client);
+
+    // create listing
+    await createListing(client, {
+      name: "Lovely Loft",
+      summary: "A charming loft in Paris",
+      bedroom: 1,
+      bathroom: 1,
+    });
   } catch (err) {
     console.error(err);
   } finally {
+    // close the database connection
     await client.close();
   }
 }
 
 main().catch(console.error);
 
+/**
+ * List Databases
+ */
 async function listDatabases(client) {
   const dbList = await client.db().admin().listDatabases();
 
   dbList.databases.forEach((db) => {
     console.log(`- ${db.name}`);
   });
+}
+
+/**
+ * Create a Listing
+ */
+async function createListing(client, newListing) {
+  const result = await client
+    .db("sample_airbnb")
+    .collection("listingsAndReviews")
+    .insertOne(newListing);
+
+  console.log(result.insertedId);
 }
